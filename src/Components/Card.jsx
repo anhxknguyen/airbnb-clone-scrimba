1abf925a6448c61b6e7a0c90c268e3714e70737d
Bon Nguyen <115838607+anhxknguyen@users.noreply.github.com>
2023-10-14 01:27:53 -0400

    Shortened props in App.jsx

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,17 +1,27 @@
 import React from "react"
 
 export default function Card(props) {
+    let badgeText
+    const card = props.card
+
+    if (props.card.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    }   else if (props.card.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
     return (
         <div className="card">
-            <img src={`../images/${props.img}`} className="card--image" />
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img src={`../images/${card.coverImg}`} className="card--image" />
             <div className="card--stats">
                 <img src="./images/star.png" className="card--star" />
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.location}</span>
+                <span>{card.stats.rating}</span>
+                <span className="gray">({card.stats.reviewCount}) • </span>
+                <span className="gray">{card.location}</span>
             </div>
-            <p>{props.title}</p>
-            <p><span className="bold">From ${props.price}</span> / person</p>
+            <p>{card.title}</p>
+            <p><span className="bold">From ${card.price}</span> / person</p>
         </div>
     )
 }
